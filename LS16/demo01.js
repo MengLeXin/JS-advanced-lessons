86bb96be885e8440de76ae8418cbe24e9e68f405
MengLeXin <970914982@qq.com>
2018-04-26 16:58:37 +0800

    2018/4/26

---
+++ LS16/demo01.js
@@ -0,0 +1,52 @@
+//一般函数中的this
+function thisTest(){
+    console.log(this === window);
+}
+thisTest();
+
+
+var a = 10;b = "Hi";
+function thisTest2(){
+    this.a = 20;
+    delete this.b;
+    this.c = "新添加属性";
+}
+thisTest2();
+console.log(a,c);
+
+//对象方法中的this
+var point = {
+    x:0,
+    y:0,
+    moveTo:function (x,y) {
+        this.x = x;
+        this.y = y;
+    }
+};
+point.moveTo(1,1);//this绑定到当前对象，即point对象上
+console.log(point);
+
+//间接调用中的this
+//call 实例1
+objA = {name:"AA",x:1};
+objB = {name:"BB",x:5};
+objA.test = function () {
+    console.log(this.name,this.x);
+};
+
+objA.test();//AA 1
+objA.test.call(objB);//BB 5
+
+//call 实例2
+var bird = {
+    name:"polly",
+    fly:function(m,n){
+        console.log("i'm:"+this.name+" i can fly ___",m,n);
+    }
+};
+
+var me = {
+    name:"QL"
+};
+bird.fly(5,6);
+bird.fly.call(me,7,8);
